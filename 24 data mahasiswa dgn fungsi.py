e96096c87eb21422860499664301bd80907ea84f
ahmfarisi <ahmfarisi@gmail.com>
2022-08-05 14:42:30 +0700

    Add extra list to existing project

--- 24 data mahasiswa dgn fungsi.py
+++ 24 data mahasiswa dgn fungsi.py
@@ -4,20 +4,26 @@ def cetakJudul():
     print('=======================')
 
 def inputDataMahasiswa(jumlah):
-    global namaMhs
+    global namaMhs, npmMhs, prodiMhs
     namaMhs = []
+    npmMhs = []
+    prodiMhs = []
+    
     for i in range(jumlah):
+        print()
         namaMhs.append(input('Nama Mahasiswa   : '))
+        npmMhs.append(input('NPM Mahasiswa    : '))
+        prodiMhs.append(input('Prodi Mahasiswa  :'))
 
 def tampilDataMahasiswa():
     global namaMhs
     
     print()
-    print('--------------------')
-    print('No\tNama')
-    print('--------------------')
+    print('------------------------------------')
+    print('No\tNama\t\tNPM\tProgram Studi')
+    print('------------------------------------')
     for i in range(len(namaMhs)):
-        print('%d\t%s' %(i+1, namaMhs[i]))
+        print('%d\t%s\t%s\t%s' %(i+1, namaMhs[i], npmMhs[i], prodiMhs[i]))
 
 #panggil
 cetakJudul()
